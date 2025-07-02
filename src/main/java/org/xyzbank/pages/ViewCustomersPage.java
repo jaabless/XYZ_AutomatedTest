199a7bf92d6e683911069e7ba4d0bdb10688913a
jaabless <jefferyasamoahbless@gmail.com>
2025-07-02 11:49:01 +0000

    feat: build ViewCustomersPage to list, search, and delete existing customers

---
+++ src/main/java/org/xyzbank/pages/ViewCustomersPage.java
@@ -0,0 +1,61 @@
+package org.xyzbank.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class ViewCustomersPage extends BasePage {
+
+    @FindBy(xpath = "//input[@placeholder='Search Customer']")
+    private WebElement searchCustomerInput;
+
+    @FindBy(xpath = "//button[contains(text(), 'Delete')]")
+    private WebElement deleteCustomerBtn;
+
+    @FindBy(xpath = "//table[@class='table table-bordered table-striped']//tbody/tr")
+    private List<WebElement> customerRows;
+
+    public ViewCustomersPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void deleteCustomer(String customerName) {
+        for (WebElement row : customerRows) {
+            if (row.getText().contains(customerName)) {
+                WebElement deleteButton = row.findElement(By.xpath(".//button[text()='Delete']"));
+                deleteButton.click();
+                break;
+            }
+        }
+    }
+
+
+//    public boolean isCustomerPresent(String customerName) {
+//        searchCustomerInput.clear();
+//        searchCustomerInput.sendKeys(customerName);
+//        return deleteCustomerBtn.isDisplayed();
+//    }
+
+    public boolean isCustomerPresent(String customerName) {
+        searchCustomerInput.clear();
+        searchCustomerInput.sendKeys(customerName);
+
+        // Wait briefly to allow filtering (could also use WebDriverWait if necessary)
+//        try {
+//            Thread.sleep(500); // optional or replace with WebDriverWait
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        for (WebElement row : customerRows) {
+            if (row.getText().contains(customerName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
