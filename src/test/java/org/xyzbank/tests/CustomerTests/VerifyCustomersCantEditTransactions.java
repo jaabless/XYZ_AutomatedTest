447dfab2d7afba297832ac38659af7e8fa5fe5a2
jaabless <jefferyasamoahbless@gmail.com>
2025-07-03 08:53:34 +0000

    feat: Verify customers cannot edit past transactions

---
+++ src/test/java/org/xyzbank/tests/CustomerTests/VerifyCustomersCantEditTransactions.java
@@ -0,0 +1,23 @@
+package org.xyzbank.tests.CustomerTests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.xyzbank.pages.Customer.CustomerDashboardPage;
+import org.xyzbank.pages.Customer.CustomerLoginPage;
+import org.xyzbank.pages.Customer.CustomerTransactionsPage;
+import org.xyzbank.tests.BaseTest;
+
+public class VerifyCustomersCantEditTransactions extends BaseTest {
+
+    @Test
+    @DisplayName("TC011: Verify customer can view recent transactions")
+    public void testTC011_ResetTransactions() {
+        CustomerLoginPage customerLoginPage = loginPage.clickCustomerLogin();
+        CustomerDashboardPage accountPage = customerLoginPage.loginAsCustomer("Hermoine Granger");
+        CustomerTransactionsPage customerTransactionsPage = accountPage.viewTransactions();
+        customerTransactionsPage.resetTransactions();
+        Assertions.assertTrue(customerTransactionsPage.isTransactionTableDisplayed(), "Recent transactions are displayed with details (Date, Amount, Transaction Type)");
+        Assertions.assertTrue(customerTransactionsPage.hasTransactions(), "Customer should have at least one transaction");
+    }
+}
