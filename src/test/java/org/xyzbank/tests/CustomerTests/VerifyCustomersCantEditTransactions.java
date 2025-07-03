bf9cd3185ccec15bf67cc322d9704fe4543ca6aa
jaabless <jefferyasamoahbless@gmail.com>
2025-07-03 16:51:04 +0000

    feat: add allure configuration

--- src/test/java/org/xyzbank/tests/CustomerTests/VerifyCustomersCantEditTransactions.java
+++ src/test/java/org/xyzbank/tests/CustomerTests/VerifyCustomersCantEditTransactions.java
@@ -7,9 +7,14 @@ import org.xyzbank.pages.Customer.CustomerDashboardPage;
 import org.xyzbank.pages.Customer.CustomerLoginPage;
 import org.xyzbank.pages.Customer.CustomerTransactionsPage;
 import org.xyzbank.tests.BaseTest;
+import io.qameta.allure.*;
 
+@Epic("Customer Banking")
+@Feature("Deposit Functionality")
 public class VerifyCustomersCantEditTransactions extends BaseTest {
 
+    @Story("Customer cannot deposit negative amount")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
     @DisplayName("TC011: Verify customer can view recent transactions")
     public void testTC011_ResetTransactions() {
